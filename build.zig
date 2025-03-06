ab2f51ce21973270434dc8de46e4ac2f9979ac45
Karl Seguin <k@openmymind.io>
2025-03-06 08:06:29 +0800

    Revert "0.14.0-dev.2851+b074fb7dd building on macos"
    This reverts commit be5dccc8b2b137c716c44a21a4fb8cda80c34414.

--- build.zig
+++ build.zig
@@ -800,5 +800,5 @@ fn statPathFromRoot(b: *std.Build, path_rel: []const u8) !PathStat {
 }
 
 fn isMinZigVersion() bool {
-    return builtin.zig_version.major == 0 and builtin.zig_version.minor == 12;
+    return builtin.zig_version.major == 0 and builtin.zig_version.minor == 14;
 }
