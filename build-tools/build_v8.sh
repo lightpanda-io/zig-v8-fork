38d6e8438f2638c538e22ed9d736c622b87d8a9a
Carson Katri <Carson.katri@gmail.com>
2025-11-13 15:13:22 -0500

    Add android build

--- build-tools/build_v8.sh
+++ build-tools/build_v8.sh
@@ -35,10 +35,55 @@ fi
 if [ "${OS}" = "ios" ]; then
   EXTRA_ARGS="v8_enable_pointer_compression=false v8_enable_webassembly=false target_environment=\"${TARGET_ENVIRONMENT}\""
 fi
+if [ "${OS}" = "android" ]; then
+  EXTRA_ARGS='
+    clang_use_chrome_plugins = false
+    is_component_build = false
+    use_dummy_lastchange = true
+    use_sysroot = false
+    simple_template_names = false
+    symbol_level = 1
+    use_debug_fission = false
+    v8_embedder_string = "-lightpanda"
+    v8_enable_sandbox = false
+    v8_enable_javascript_promise_hooks = true
+    v8_promise_internal_field_count = 1
+    v8_use_external_startup_data = false
+    v8_imminent_deprecation_warnings = false
+    v8_enable_private_mapping_fork_optimization = true
+    v8_use_zlib = true
+    v8_enable_snapshot_compression = false
+    v8_enable_handle_zapping = false
+    v8_typed_array_max_size_in_heap = 0
+    v8_array_buffer_internal_field_count = 2
+    v8_array_buffer_view_internal_field_count = 2
+    v8_enable_verify_heap = false
+    v8_enable_fuzztest = false
+    v8_enable_v8_checks = false
+    use_relative_vtables_abi = false
+    icu_use_data_file = false
+    v8_enable_temporal_support = false
+    use_llvm_libatomic = false
+    target_os = "android"
+    target_cpu = "arm64"
+    v8_target_cpu = "arm64"
+    is_debug = false
+    v8_enable_pointer_compression=false
+    v8_enable_webassembly=false
+    is_official_build=false
+
+    v8_target_cpu="arm64"
+    v8_enable_pointer_compression=false
+    v8_enable_webassembly=false
+  '
+fi
 
 TARGET_ARCH=${ARCH}
 if [ "${ARCH}" = "amd64" ]; then
   TARGET_ARCH="x64"
+fi
+if [ "${ARCH}" = "aarch64" ]; then
+  TARGET_ARCH="arm64"
 fi
 
 tools/gn \
@@ -49,7 +94,7 @@ tools/gn \
   --args="
     target_os=\"${OS}\"
     target_cpu=\"${TARGET_ARCH}\"
-    host_cpu=\"${TARGET_ARCH}\"
+    host_cpu=\"${HOST_ARCH}\"
     is_debug=${IS_DEBUG}
     symbol_level=${SYMBOL_LEVEL}
     is_official_build=false ${EXTRA_ARGS}
